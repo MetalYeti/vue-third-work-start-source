1a645518f211d44979457f5a885c2b8494af5b30
Andrey Bezrukov <bandrei@bk.ru>
2024-08-20 11:47:24 +0400

    First commit

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<template>
+    <main class="content">
+        <section class="desk">
+            <!--      Шапка доски-->
+
+            <!--      Колонки и задачи-->
+
+            <!--      Пустая доска-->
+        </section>
+    </main>
+</template>
